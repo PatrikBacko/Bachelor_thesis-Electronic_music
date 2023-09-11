21fc56d8700f32edebf7a22a804f5338951a0062
Buciak <patrik.backo1@gmail.com>
2023-09-11 20:57:23 +0200

    Downloaded 70 gb of data, about to sort them into samples.
    Added work in progress data sorting scripts

---
+++ random_scripts/data_sorting_script/main.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import re
+from visitors import IVisitor, RemoveVisitor, Visitor1
+
+def remove_dir_with_content(root_path: str):
+    process_content_recursively(root_path, RemoveVisitor())
+    os.rmdir(root_path)
+
+def process_content_recursively(root_path: str, visitor: IVisitor):
+    for name in os.listdir(root_path):
+        path = os.path.join(root_path, name)
+        if os.path.isdir(path):
+            if visitor.dir_predicate(path):
+                process_content_recursively(path, visitor)
+            visitor.dir_action(path)
+        else:
+            visitor.file_action(path)
+    
+def main():
+    path = r"C:\Users\llama\Downloads\xdd"
+    visitor = Visitor1()
+    process_content_recursively(path, visitor)
+
+if __name__ == "__main__":
+    main()
+
---
+++ random_scripts/data_sorting_script/visitors.py
@@ -0,0 +1,26 @@
+class IVisitor:
+    def file_action(self, path: str):
+        pass
+    def dir_action(self, path):
+        pass
+    def dir_predicate(self, path: str) -> bool:
+        return True
+
+class RemoveVisitor(IVisitor):
+    def file_action(self, path: str):
+        os.remove(path)
+    def dir_action(self, path):
+        os.rmdir(path)
+    def dir_predicate(self, path: str) -> bool:
+        return True
+
+class Visitor1(IVisitor):
+    def file_action(self, path: str):
+        pass
+    def dir_action(self, path):
+        pass
+    def dir_predicate(self, path: str) -> bool:
+        if os.path.basename(path) == "__MACOSX":
+            remove_dir_with_content(path)
+            return False
+        return True
