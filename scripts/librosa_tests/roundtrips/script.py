a5cf322c2f970e837ff8887c96e5f1ad257a5eee
Buciak <patrik.backo1@gmail.com>
2023-12-03 12:41:10 +0100

    Delete unnecessary files and code

--- scripts/librosa_tests/roundtrips/script.py
+++ scripts/librosa_tests/roundtrips/script.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 This script is used to create a round trip (wav -> extract features -> (optionally) add noise -> wav) and save the results to a .wav file. (or a batch of files when given a directory)
 The scripts tries several feature extraction methods:
@@ -18,8 +17,6 @@ Types of noise:
     -- pixel 
     -- time step (column)
     -- frequency step (row)
-
-
 """
 
 
@@ -38,6 +35,7 @@ import soundfile as sf
 import pyaudio
 
 import argparse
+import os
 
 FEATURE_EXTRACTION_METHODS = ["mfcc", "mel", "stft"]
 
@@ -56,7 +54,7 @@ def build_arguments() -> argparse.ArgumentParser:
     #argument for output path to wav file or directory with wav files
     parser.add_argument("output", help="output path to a directory for output wav files", default="./output")
 
-
+    #argument for noise
     parser.add_argument("-n", "--noise", help="add noise to the spectogram", action="store_true", default=False)
 
     #argument for noise type
@@ -90,6 +88,20 @@ def build_arguments() -> argparse.ArgumentParser:
                         "mel: Mel Spectogram"
                         "stft: Short-time Fourier transform"
                         , choices=FEATURE_EXTRACTION_METHODS, default="mfcc")
+    
+
+    #TODO: both spectogram and wave plotting in the future ? 
+    parser.add_argument("-t", "--plot", help="plot the spectogram or waves, depending on settings (original, roundtripped and noised)", choices=["spectogram", "waves", "both"], default=False)
+    parser.add_argument("-p", "--play", help="play the original, roundtripped and noised waves", action="store_true", default=False)
+
+    #TODO: batch ??????
+    parser.add_argument("-b", "--batch", help="process a batch of files", action="store_true", default=False)
+
+    #TODO: save ???
+    parser.add_argument("-s", "--save", help="save rountrip with or without noise (or both)", choices=["noised", "round_trip", "both"], default=False)
+
+
+    return parser
 
 
 # * kwargs
@@ -167,12 +179,33 @@ def save_lb_wave(wave: np.ndarray, sample_rate: int, output_file_path: str) -> N
     """
     lb.output.write_wav(output_file_path, wave, sample_rate)
 
+def play_wave(wave: np.ndarray, sample_rate: int) -> None:
+    """
+    Play wave data using pyaudio
+
+    params:
+        - wave: wave data
+        - sample_rate: wave sample rate
+    """
+
+    p = pyaudio.PyAudio()
+
+    # open a stream
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=sample_rate,
+                    output=True)
+
+    stream.write(wave.tobytes())
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
 
 
 
 #mfcc
-def get_mfcc(wave: np.ndarray, sample_rate: int) -> np.ndarray:
+def get_mfcc(wave: np.ndarray, sample_rate: int, **kwargs) -> np.ndarray:
     """
     Extract mfcc features from wave data
 
@@ -183,10 +216,10 @@ def get_mfcc(wave: np.ndarray, sample_rate: int) -> np.ndarray:
     returns:
         - mfcc spectogram
     """
-    return lb.feature.mfcc(y=wave, sr=sample_rate)
+    return lb.feature.mfcc(y=wave, sr=sample_rate, **kwargs)
 
 
-def mfcc_roundtrip(wave: np.ndarray, sample_rate: int, noise_function: callable) -> np.ndarray:
+def mfcc_roundtrip(wave: np.ndarray, sample_rate: int, noise_function: callable, **kwargs) -> np.ndarray:
     """
     Create a round trip (wav -> extract features -> add noise -> wav) using mfcc feature extraction method
 
@@ -199,7 +232,7 @@ def mfcc_roundtrip(wave: np.ndarray, sample_rate: int, noise_function: callable)
         - round trip wave data
     """
     #extract mfcc features
-    mfcc = get_mfcc(wave, sample_rate)
+    mfcc = get_mfcc(wave, sample_rate, **kwargs)
     #add noise to the spectogram
     mfcc_noised = noise_function(mfcc)
     #invert the mfcc spectogram
@@ -296,25 +329,189 @@ def stft_roundtrip(wave: np.ndarray, sample_rate: int, noise_function: callable)
     return lb.istft(stft_noised, sr = sample_rate)
 
 
+
+def normal_noise(mean: float, variance: float, shape: tuple[int, int], dtype = np.float32) -> np.ndarray:
+    """
+    Generate normal noise with given mean and variance
+
+    params:
+        - mean: mean of the normal distribution
+        - variance: variance of the normal distribution
+        - shape: shape of the noise array
+    
+    returns:
+        - noise array
+    """
+    return np.random.normal(mean, variance, shape, dtype=dtype)
+
+def uniform_noise(mean: float, variance: float, shape: tuple[int, int], dtype=np.float32) -> np.ndarray:
+    """
+    Generate uniform noise with given mean and variance
+
+    params:
+        - mean: mean of the uniform distribution
+        - variance: variance of the uniform distribution
+        - shape: shape of the noise array
+
+    returns:
+        - noise array
+    """
+    return np.random.uniform(mean-variance, mean+variance, shape, dtype=dtype)
+
+def constant_noise(mean: float, variance: float, shape: tuple[int, int], dtype = np.float32) -> np.ndarray:
+    """
+    Generate constant noise with given mean and variance
+
+    params:
+        - mean: mean of the constant distribution
+        - variance: variance of the constant distribution
+        - shape: shape of the noise array 
+
+    returns:
+        - noise array
+    """
+    return np.full(shape, mean, dtype=dtype)
+
+
+def add_noise(spectogram, noise) -> callable: 
+    """
+    Add noise to the spectogram
+
+    params:
+        - spectogram: spectogram to add noise to
+        - noise: noise array
+    
+    returns:
+        - spectogram with added noise
+    """
+    return spectogram + noise
+
+def multiply_noise(spectogram, noise) -> callable:
+    """
+    Multiply the spectogram with noise
+
+    params:
+        - spectogram: spectogram to multiply with noise
+        - noise: noise array
+    
+    returns:
+        - spectogram multiplied with noise
+    """
+    return spectogram * noise
+
+def generate_noise(args) -> callable:
+    """
+    Generate noise function based on the given arguments
+
+    params:
+        - args: arguments for generating noise function
+
+    returns:
+        - noise function
+    """
+
+    #noise generating distribution
+    if args.distribution == "normal":
+        distribution = lambda shape: normal_noise(args.mean, args.variance, shape)
+    elif args.distribution == "uniform":
+        distribution = lambda shape: uniform_noise(args.mean, args.variance, shape)
+    elif args.distribution == "constant":
+        distribution = lambda shape: constant_noise(args.mean, args.variance, shape)
+
+    #noise scope
+    if args.scope == "pixel":
+        scope = lambda spectogram: distribution(spectogram.shape)
+    elif args.scope == "column":
+        scope = lambda spectogram: np.outer(distribution(spectogram.shape[1]), np.zeros(spectogram.shape[0]))
+    elif args.scope == "row":
+        scope = lambda spectogram: np.outer(np.zeros(spectogram.shape[1]), distribution(spectogram.shape[0]))
+    elif args.scope == "entire_picture":
+        scope = lambda spectogram: np.full(spectogram.shape, distribution((1,))[0])
+
+    #noise operation type
+    if args.operation == "additive":
+        noise_function = lambda spectogram: add_noise(spectogram, scope(spectogram))
+    elif args.operation == "multiplicative":
+        noise_function = lambda spectogram: multiply_noise(spectogram, scope(spectogram))
+
+    return noise_function
+
+
+
+def plot_waves(wave: np.ndarray, round_trip: np.ndarray, round_trip_noised: np.ndarray,  sample_rate: int) -> None:
+
+    plt.figure(figsize=(14, 5))
+
+    plt.subplot(1, 3, 1)
+    librosa.display.waveplot(wave, sr=sample_rate)
+    plt.title("Original Wave")
+
+    plt.subplot(1, 3, 2)
+    librosa.display.waveplot(round_trip, sr=sample_rate)
+    plt.title("Round Trip Wave")
+
+    plt.subplot(1, 3, 3)
+    librosa.display.waveplot(round_trip_noised, sr=sample_rate)
+    plt.title("Noised Round Trip Wave")
+
+    plt.show()
+
+def plot_spectograms(spectogram_1, spectogram_2) -> None:
+    #TODO: implement
+    raise NotImplementedError("Spectogram plotting is not implemented yet")
+
+
 def main(args):
     #load wave file
     wave, sample_rate = load_lb_wave(args.input)
 
-    noise_function = lambda x: x
+    no_noise = lambda x: x
+
+    #TODO: add noise functions
+    if args.noise:
+        noise_function = generate_noise(args)
+    else:
+        noise_function = no_noise
+
 
     if args.feature == "mfcc":
-        round_trip = mfcc_roundtrip(wave, sample_rate, noise_function)
+        round_trip_noised = mfcc_roundtrip(wave, sample_rate, noise_function)
+        round_trip = mfcc_roundtrip(wave, sample_rate, no_noise)
+
     elif args.feature == "mel":
-        round_trip = mel_spectogram_roundtrip(wave, sample_rate, noise_function)
+        round_trip_noised = mel_spectogram_roundtrip(wave, sample_rate, noise_function)
+        round_trip = mel_spectogram_roundtrip(wave, sample_rate, no_noise)
+
     elif args.feature == "stft":
-        round_trip = stft_roundtrip(wave, sample_rate, noise_function)
+        round_trip_noised = stft_roundtrip(wave, sample_rate, noise_function)
+        round_trip = stft_roundtrip(wave, sample_rate, no_noise)
 
-    #save round trip wave file
-    save_lb_wave(round_trip, sample_rate, args.output)
 
+    if args.plot == "spectogram":
+        plot_spectograms(round_trip, round_trip_noised)
+    elif args.plot == "waves":
+        plot_waves(wave, round_trip, round_trip_noised, sample_rate)
+    elif args.plot == "both":
+        raise NotImplementedError("Both plotting is not implemented yet") 
+
+    if args.play:
+        #play waves
+        play_wave(wave, sample_rate)
+        play_wave(round_trip, sample_rate)
+        play_wave(round_trip_noised, sample_rate)
+    
+    #TODO: save round trip wave file
+    # if args.save:
+    #     if args.save
+    #     save_lb_wave(round_trip, sample_rate, args.output)
+
+    if args.save == "noised":
+        save_lb_wave(round_trip_noised, sample_rate, args.output +"\\" +os.path.basename(args.input) + "_noised.wav")
+    elif args.save == "round_trip":
+        pass
 
 
 if __name__ == "__main__":
-    pass
-    # parser = argparse.ArgumentParser(description='Process some integers.')
-    # args = parser.parse_args()
+    args = build_arguments().parse_args()
+
+    main(args)
--- scripts/test.py
+++
@@ -1,6 +0,0 @@
-import os
-import django
-
-path = r"C:\Users\llama\Desktop\programming shit\Bakalarka\Bakalaris-data\drums-one_shots"
-
-print(list(map(lambda path: os.path.basename(path), os.listdir(path))))
