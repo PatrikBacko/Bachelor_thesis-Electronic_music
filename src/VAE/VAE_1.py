e57c1b62e50fe1c7649db9a963b88f7e16e53f37
Patrik Backo <patrik.backo1@gmail.com>
2024-01-14 18:37:53 +0100

    renaming  directories
    addind script to train model on metacentrum

---
+++ src/VAE/VAE_1.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Encoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(Encoder, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=2, padding=0)
+        self.fc = nn.Linear(16 * 63 * 24, latent_dim)
+        self.fc_mu = nn.Linear(latent_dim, latent_dim)
+        self.fc_logvar = nn.Linear(latent_dim, latent_dim)
+
+    def forward(self, x):
+        # print(f"Encoder input shape: {x.shape}")
+        x = F.relu(self.conv1(x))
+        # print(f"Encoder shape after conv1: {x.shape}")
+        x = F.relu(self.conv2(x))
+        # print(f"Encoder shape after conv2: {x.shape}")
+        x = nn.Flatten()(x)
+        # print(f"Encoder shape after flatten: {x.shape}")
+        x = F.relu(self.fc(x))
+        # print(f"Encoder shape after fc: {x.shape}")
+        mu = self.fc_mu(x)
+        logvar = self.fc_logvar(x)
+        return mu, logvar
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(Decoder, self).__init__()
+        self.fc = nn.Linear(latent_dim, 16 * 63 * 24)
+        self.deconv1 = nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=3, stride=2, padding=0)
+        self.deconv2 = nn.ConvTranspose2d(in_channels=8, out_channels=1, kernel_size=3, stride=2, padding=0, output_padding=1)
+
+    def forward(self, x):
+        # print()
+        # print(f"Dencoder input shape: {x.shape}")
+        x = F.relu(self.fc(x))
+        # print(f"Decoder shape after fc: {x.shape}")
+        x = x.view(-1, 16, 63, 24)
+        # print(f"Decoder shape after view: {x.shape}")
+        x = F.relu(self.deconv1(x))
+        # print(f"Decoder shape after deconv1: {x.shape}")
+        x = self.deconv2(x)
+        # print(f"Decoder shape after deconv2: {x.shape}")
+
+        #Mirroring of weights with encoder
+        # F.ConvTranspose(weight = endocer layers ...)
+        
+        return x
+
+class VAE_1(nn.Module):
+    def __init__(self, latent_dim):
+        super(VAE_1, self).__init__()
+        self.encoder = Encoder(latent_dim)
+        self.decoder = Decoder(latent_dim)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def forward(self, x):
+        mu, logvar = self.encoder(x)
+        z = self.reparameterize(mu, logvar)
+        reconstructed_x = self.decoder(z)
+        return reconstructed_x, mu, logvar
---
+++ src/VAE/train_model.py
@@ -0,0 +1,149 @@
+'''
+Script for training the model (Variational Autoencoder) on given samples and saving it.
+'''
+
+import math
+import os
+import numpy as np
+import librosa as lb
+import soundfile as sf
+import pyaudio
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .VAE_1 import VAE_1
+
+import argparse
+
+def build_arguments():
+    '''
+    builds arguments for the script
+    '''
+    
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('source_dir', type=str, help='Path to directory with samples.')
+    parser.add_argument('model_path', type=str, help='Path to save model.')
+    parser.add_argument('--latent_dim', type=int, default=32, help='Latent dimension of the model.')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train the model.')
+
+    return parser.parse_args()
+
+
+
+def loss_function(reconstructed_x, x, mu, logvar):
+    '''
+    loss function for VAE with MSE loss as reconstruction loss and KL divergence (makes autoencoder variational)
+
+    params:
+        reconstructed_x - reconstructed x
+        x - original x
+        mu - mean
+        logvar - log variance
+
+    returns:
+        loss - loss of the model
+    '''
+    reconstruction_loss = F.mse_loss(reconstructed_x, x, reduction='sum') #mse for simplicity, could change in the future
+    kl_divergence = - 0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return (reconstruction_loss + kl_divergence)
+
+
+def train(model, train_loader, epochs, device):
+    '''
+    trains the model
+    '''
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    model.train()
+
+    losses = []
+
+    for epoch in range(epochs):
+        train_loss = 0
+        for batch_idx, x in enumerate(train_loader):
+            x = x.to(device)
+            optimizer.zero_grad()
+            reconstructed_x, mu, logvar = model(x)
+            loss = loss_function(reconstructed_x, x, mu, logvar)
+            loss.backward()
+            train_loss += loss.item()
+            optimizer.step()
+
+        average_loss = train_loss / len(train_loader.dataset)
+        print('====> Epoch: {} Average loss: {:.4f}'.format(epoch+1, average_loss))
+        losses.append(average_loss)
+    print('Finished training.') 
+
+    return losses
+    
+def pad_or_trim(mfcc, length = 100):
+    '''
+    pads or trims mfcc to given length, default is 100 ! (cca 1 second with 256 hop length and 512 n_fft and 44100 sr) !
+
+    params:
+        mfcc - mfcc to pad or trim
+        length - length to pad or trim to (default is 100)
+
+    returns:
+        mfcc - padded or trimmed mfcc
+    '''
+
+    if mfcc.shape[1] > length:
+        return mfcc[:, :length]
+    else:
+        last_column = mfcc[:, -1:]
+        padding = np.repeat(last_column, length - mfcc.shape[1], axis=1)
+        return np.concatenate((mfcc, padding), axis=1)
+
+def prepare_data(source_dir, length = 100):
+    '''
+    gets path to directory with samples, and returns a dataloader with padded or trimmed mfccs of the samples
+    ! padded or trimmed to cca 1 second with 256 hop length and 512 n_fft and 44100 sr !
+
+    params:
+        source_dir - path to directory with samples
+        length - length to pad or trim to (default is 100)
+
+    returns:
+        train_loader - dataloader with padded or trimmed mfccs of the samples
+    '''
+
+    paths_to_samples = [os.path.join(source_dir, path) for path in os.listdir(source_dir)]
+
+    mfccs = []
+
+    for path in paths_to_samples:
+            array, sr = lb.load(path)
+
+            mfcc = lb.feature.mfcc(y=array, sr=sr, n_mfcc=512, n_fft=512, hop_length=256, lifter=0, dct_type=3, n_mels = 256)
+            mfcc_pad_or_trim = pad_or_trim(mfcc, 100)
+
+            mfccs.append(mfcc_pad_or_trim)
+
+    mfccs_tensor = torch.tensor(mfccs).view(-1, 1, 256, 100)
+    train_loader = torch.utils.data.DataLoader(mfccs_tensor, batch_size=4, shuffle=True)
+
+    return train_loader
+    
+
+def main(args):
+
+    model = VAE_1(latent_dim)
+
+    train_loader = prepare_data(args.source_dir, length = 100)
+
+    latent_dim = args.latent_dim
+    epochs = args.epoch
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    losses = train(model, train_loader, epochs, device)
+
+    torch.save(model.state_dict(), args.model_path)
+
+
+if __name__ == '__main__':
+    args = build_arguments()
+    main(args)
