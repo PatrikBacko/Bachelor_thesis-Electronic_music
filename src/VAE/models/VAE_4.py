5d2b9645cefd828836886e04bfbead5c5b16371d
PatrikBacko <patrik.backo1@gmail.com>
2024-04-01 17:22:55 +0200

    .

--- src/VAE/models/VAE_4.py
+++ src/VAE/models/VAE_4.py
@@ -97,7 +97,7 @@ class Decoder(nn.Module):
         )
 
         self.block_3 = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1, output_padding=1),
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
